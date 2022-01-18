9cb9978c39fd16518eb8ba88d59f43fde2572ac8
cedricngoune <gabyngoune@yahoo.fr>
2022-01-18 22:05:52 +0100

    Add new line separator

--- string-calculator/add.spec.ts
+++ string-calculator/add.spec.ts
@@ -32,6 +32,16 @@ describe("String calculator", () => {
       stringNumbers: "10,2,1",
       expectedResult: 13,
     },
+    {
+      caseMsg: "It should add many numbers",
+      stringNumbers: "10,2,1,1,2",
+      expectedResult: 16,
+    },
+    {
+      caseMsg: "It should add new line separator",
+      stringNumbers: "1,2\n3",
+      expectedResult: 6,
+    },
   ])("$caseMsg", async (args) => {
     const { stringNumbers, expectedResult } = args
     const result: number = stringCalculator.add(stringNumbers)
--- string-calculator/add.ts
+++ string-calculator/add.ts
@@ -1,10 +1,14 @@
 export const stringCalculator = {
   add: (stringNumber: string): number => {
     let result = 0
-    let values: string[] = stringNumber.split(",")
+    const lines = stringNumber.split("\n")
+    const values: string[] = lines[0].split(",")
 
     for (const value of values) {
       result += Number(value)
+    }
+    if (lines[1]) {
+      result += stringCalculator.add(lines[1])
     }
     return result
   },
